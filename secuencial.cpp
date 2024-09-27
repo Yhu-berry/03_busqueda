d3cfdef71506f0c0911e8547d4bc3953daf449dc
Yhu-berry <ynmaqueraf@unjbg.edu.pe>
2024-09-27 17:20:21 -0300

    Agregando la funcion secuencialOrd

--- secuencial.cpp
+++ secuencial.cpp
@@ -3,8 +3,9 @@ using namespace std;
 
 // Funcion que busca en todo el arreglo (desordenado)
 int secuencialDes(int[100],int, int);
+int secuencialOrd(int[100],int,int);
 int main(){
-    int Arr[100], num,datoAbuscar,posicion;
+    int Arr[100], num,datoAbuscar,posicionOrd,posicionDes;
     cout<<"Ingrese el tamaño del arreglo: ";
     cin>>num;
 
@@ -16,8 +17,10 @@ int main(){
     cin>>datoAbuscar;
 
     //Mostrar la posicion del arreglo hallado
-    posicion=secuencialDes(Arr,num,datoAbuscar);
-    cout<<posicion;
+    /*posicionDes=secuencialDes(Arr,num,datoAbuscar);
+    cout<<posicionDes;*/
+    posicionOrd=secuencialOrd(Arr,num,datoAbuscar);
+    cout<<posicionOrd;
 
     return 0;
 }
@@ -32,5 +35,21 @@ int secuencialDes(int A[100],int n,int dato){
     if(i<n){
         pos=i;
     }
+    return pos;
+}
+
+int secuencialOrd(int A[100],int n, int dato){
+    int pos,i;
+    i=0;
+    while(i<n && A[i]<dato){
+        i=i+1;
+    }
+    //Si quiero buscar un numero menor no funciona
+    if(i>n || A[i]!=dato){
+        pos=-i;
+    }else{
+        pos=i;
+    }
+
     return pos;
 }
